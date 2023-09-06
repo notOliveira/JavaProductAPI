071219e98a8aa41634cca7cbd539373534ea4f0d
Gustavo Oliveira <106312771+notOliveira@users.noreply.github.com>
2023-09-06 10:26:30 -0300

    Adicionando @CrossOrigin

--- produto/src/main/java/com/fatec/produto/controller/APIProdutoController.java
+++ produto/src/main/java/com/fatec/produto/controller/APIProdutoController.java
@@ -4,6 +4,7 @@ import com.fatec.produto.services.IProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class APIProdutoController {
     @Autowired
     IProdutoService produtoServico;
 
+    @CrossOrigin
     @GetMapping("api/v1/produtos")
     public ResponseEntity <Object> getAll() {
         return ResponseEntity.status(HttpStatus.OK).body(produtoServico.consultaCatalogo());

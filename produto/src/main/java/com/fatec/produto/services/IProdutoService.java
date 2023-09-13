999de77f7cfe4b6fb8411dd8fbb14efa48c6a82e
Gustavo Oliveira <Aluno@fatecdiadema.com>
2023-09-13 08:19:12 -0300

    Ajustando repositório

--- produto/src/main/java/com/fatec/produto/services/IProdutoService.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.produto.services;
-
-import com.fatec.produto.model.entities.Produto;
-
-import java.util.List;
-
-public interface IProdutoService {
-    public List<Produto> consultaCatalogo();
-}
